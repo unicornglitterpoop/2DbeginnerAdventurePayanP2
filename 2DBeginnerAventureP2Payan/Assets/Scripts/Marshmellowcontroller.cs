54a7bda2c5e66597304b23fde238e37befbfce08
unicornglitterpoop <761368@student.dusd.net>
2024-10-22 10:02:57 -0700

    Made my character move
    epic

---
+++ 2DBeginnerAventureP2Payan/Assets/Scripts/Marshmellowcontroller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Marshmellowcontroller : MonoBehaviour
+{
+
+    public float speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log(horizontal);
+
+        Vector2 position = transform.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        transform.position = position;
+    }
+}
